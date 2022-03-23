cd080be1bd2be13814d358b2bd2dda88be8be2ed
takuya sakaguchi <takuya1002@gmail.com>
2022-03-24 00:25:59 +0900

    init func, func, main func

--- lesson.go
+++ lesson.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+// 一番最初に呼ばれる特別な関数
+func init() {
+	fmt.Println("Init")
+}
+
+// 通常の関数はmain関数から呼ぶ必要がある
+func buzz() {
+	fmt.Println("Buzz")
+}
+
+// main関数
 func main() {
-	fmt.Println("Hello, World!")
+	buzz()
+	fmt.Println("Hello, World!", "test")
 }
